6d7119c09bbaa833f2a193912aad7058a8ddd1ec
Santiago Agudelo <sagudelo1200@gmail.com>
2020-11-25 22:41:44 -0500

    added class method to get one or all collections in database

---
+++ models/engine/firebase_storage.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+''' contains the firebase storage engine '''
+import firebase_admin
+from firebase_admin import credentials, firestore
+from os import getenv
+from models import collections
+
+cred = getenv('QUIMBAYAS_API_CREDENTIALS')
+
+
+class FirebaseStorage:
+    '''  '''
+    __engine = None
+    __db = None
+
+    def __init__(self):
+        ''' Validates the firebase sdk credentials and start de client '''
+        if not cred:
+            print('QUIMBAYAS_API_CREDENTIALS: Invalid doc or not found')
+            exit(-1)
+
+        app = credentials.Certificate(cred)
+        self.__engine = firebase_admin.initialize_app(app)
+        self.__db = firestore.client()
+
+    def all(self, col=None):
+        '''
+        query on te current firestorage session and get all docs
+        in the indicated collection or in all collections
+        '''
+        data = {}
+
+        if col and col not in collections:
+            return {'error': 'Invalid collection'}
+
+        for _col in collections:
+            if not col or col == _col:
+                try:
+                    docs = self.__db.collection(_col).stream()
+
+                    for doc in docs:
+                        obj = doc.to_dict()
+                        key = f'{_col}.{doc.id}'
+
+                        id = obj.get('id')
+                        if id and id != doc.id:
+                            print(f'The document {key} contains an id field with a different value ({id}) than its actual id')
+
+                        obj['id'] = doc.id
+                        data[key] = obj
+                except Exception as e:
+                    print(e)
+
+        return data
+
+
+if __name__ == "__main__":
+    db = DBStorage()
+
+    print(db.all())
