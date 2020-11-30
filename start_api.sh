3f9acce491b580486a3c86f8918f51a6454f073e
Santiago Agudelo <sagudelo1200@gmail.com>
2020-11-30 14:04:51 -0500

    export API_SECRET_KEY

--- start_api.sh
+++ start_api.sh
@@ -3,5 +3,6 @@ export QUIMBAYAS_TYPE_STORAGE=fb # firebase
 export QUIMBAYAS_API_CREDENTIALS=firebase-sdk.json
 export QUIMBAYAS_API_HOST=0.0.0.0
 export QUIMBAYAS_API_PORT=7000
+export API_SECRET_KEY=581495d8900840f98532f0d245475d
 
 python3 -m api.v1.app
